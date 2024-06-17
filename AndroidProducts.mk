6fe1877d0f466f759306cacee0dc58c920e6d0e4
wuwenhan2010 <117785942+wuwenhan2010@users.noreply.github.com>
2024-06-17 13:31:44 +0800

    update

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_tb8768tp1_64_bsp.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_tb8768tp1_64_bsp-user \
+    omni_tb8768tp1_64_bsp-userdebug \
+    omni_tb8768tp1_64_bsp-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for alps tb8768tp1_64_bsp (tb8768tp1_64_bsp)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
